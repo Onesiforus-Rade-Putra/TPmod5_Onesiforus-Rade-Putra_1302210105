0718bab18210a33c94c2e789d629252602576dcb
Onesiforus-Rade-Putra <onesiforusradeputra@gmail.com>
2023-03-19 20:28:05 +0700

    menambahakan class HaloGeneric dengan method SapaUser' cancel end ' ; '

--- TPModul5_1302210105/Program.cs
+++ TPModul5_1302210105/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class HaloGeneric
+{
+    public void SapaUser<R>(R user)
+    {
+        Console.WriteLine("Hallo User " + user);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric haloR = new HaloGeneric();
+        haloR.SapaUser("Onesiforus Rade Putra");
+    }
+}
